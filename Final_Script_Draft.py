807d26fad5f15915dfd1d82cf60b6c9da03bc273
Nerbe16 <156106766+Nerbe16@users.noreply.github.com>
2024-05-27 15:40:21 -0500

    code code code code

--- Final_Script_Draft.py
+++ Final_Script_Draft.py
@@ -6,66 +6,44 @@ import math
 from pymavlink import mavutil
 import csv
 
-#Set up option parsing to get connection string
-import argparse  
-parser = argparse.ArgumentParser(description='Demonstrates basic mission operations.')
-parser.add_argument('--connect', 
-                   help="vehicle connection target string. If not specified, SITL automatically started and used.")
-args = parser.parse_args()
-
-connection_string = args.connect
-sitl = None
-
-
-#Start SITL if no connection string specified
-if not connection_string:
-    import dronekit_sitl
-    sitl = dronekit_sitl.start_default()
-    connection_string = sitl.connection_string()
-
-
-# Connect to the Vehicle
-print('Connecting to vehicle on: %s' % connection_string)
-vehicle = connect(connection_string, wait_ready=True)
-
-def arm_and_takeoff(aTargetAltitude):
+def arm_and_takeoff_death(aTargetAltitude):
     """
     Arms vehicle and fly to aTargetAltitude.
     """
 
     print("Basic pre-arm checks")
     # Don't let the user try to arm until autopilot is ready
-    while not vehicle.is_armable:
+    while not Death.is_armable:
         print(" Waiting for vehicle to initialise...")
         time.sleep(1)
 
         
     print("Arming motors")
     # Copter should arm in GUIDED mode
-    vehicle.mode = VehicleMode("GUIDED")
-    vehicle.armed = True
+    Death.mode = VehicleMode("GUIDED")
+    Death.armed = True
 
-    while not vehicle.armed:      
+    while not Death.armed:      
         print(" Waiting for arming...")
         time.sleep(1)
 
     print("Taking off!")
-    vehicle.simple_takeoff(aTargetAltitude) # Take off to target altitude
+    Death.simple_takeoff(aTargetAltitude) # Take off to target altitude
 
     # Wait until the vehicle reaches a safe height before processing the goto (otherwise the command 
     #  after Vehicle.simple_takeoff will execute immediately).
     while True:
-        print(" Altitude: ", vehicle.location.global_relative_frame.alt)      
-        if vehicle.location.global_relative_frame.alt>=aTargetAltitude*0.95: #Trigger just below target alt.
+        print(" Altitude: ", Death.location.global_relative_frame.alt)      
+        if Death.location.global_relative_frame.alt>=aTargetAltitude*0.95: #Trigger just below target alt.
             print("Reached target altitude")
             break
         time.sleep(1)
 
-def send_global_velocity(velocity_x, velocity_y, velocity_z, duration):
+def send_global_velocity_death(velocity_x, velocity_y, velocity_z, duration):
     """
     Move vehicle in direction based on specified velocity vectors.
     """
-    msg = vehicle.message_factory.set_position_target_global_int_encode(
+    msg = Death.message_factory.set_position_target_global_int_encode(
         0,       # time_boot_ms (not used)
         0, 0,    # target system, target component
         mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT_INT, # frame
@@ -82,77 +60,16 @@ def send_global_velocity(velocity_x, velocity_y, velocity_z, duration):
 
     # send command to vehicle on 1 Hz cycle
     for x in range(0,duration):
-        vehicle.send_mavlink(msg)
+        Death.send_mavlink(msg)
         time.sleep(1)
-
-def get_location_metres(original_location, dNorth, dEast):
-    """
-    Returns a LocationGlobal object containing the latitude/longitude `dNorth` and `dEast` metres from the
-    specified `original_location`. The returned LocationGlobal has the same `alt` value
-    as `original_location`.
-
-    The function is useful when you want to move the vehicle around specifying locations relative to
-    the current vehicle position.
-
-    The algorithm is relatively accurate over small distances (10m within 1km) except close to the poles.
-
-    For more information see:
-    http://gis.stackexchange.com/questions/2951/algorithm-for-offsetting-a-latitude-longitude-by-some-amount-of-meters
-    """
-    earth_radius=6378137.0 #Radius of "spherical" earth
-    #Coordinate offsets in radians
-    dLat = dNorth/earth_radius
-    dLon = dEast/(earth_radius*math.cos(math.pi*original_location.lat/180))
-
-    #New position in decimal degrees
-    newlat = original_location.lat + (dLat * 180/math.pi)
-    newlon = original_location.lon + (dLon * 180/math.pi)
-    if type(original_location) is LocationGlobal:
-        targetlocation=LocationGlobal(newlat, newlon,original_location.alt)
-    elif type(original_location) is LocationGlobalRelative:
-        targetlocation=LocationGlobalRelative(newlat, newlon,original_location.alt)
-    else:
-        raise Exception("Invalid Location object passed")
-
-    return targetlocation;
-
-def get_distance_metres(aLocation1, aLocation2):
-    """
-    Returns the ground distance in metres between two `LocationGlobal` or `LocationGlobalRelative` objects.
-
-    This method is an approximation, and will not be accurate over large distances and close to the
-    earth's poles. It comes from the ArduPilot test code:
-    https://github.com/diydrones/ardupilot/blob/master/Tools/autotest/common.py
-    """
-    dlat = aLocation2.lat - aLocation1.lat
-    dlong = aLocation2.lon - aLocation1.lon
-    return math.sqrt((dlat*dlat) + (dlong*dlong)) * 1.113195e5
-
-def goto(dNorth, dEast, gotoFunction = vehicle.simple_goto):
-    currentLocation=vehicle.location.global_relative_frame
-    targetLocation=get_location_metres(currentLocation, dNorth, dEast)
-    targetDistance=get_distance_metres(currentLocation, targetLocation)
-    gotoFunction(targetLocation)
-    x = 0
-    while vehicle.mode.name=="GUIDED": #Stop action if we are no longer in guided mode.
-        remainingDistance=get_distance_metres(vehicle.location.global_frame, targetLocation)
-        print ("Distance to target: ", remainingDistance)
-        gotoFunction(targetLocation)
-        if remainingDistance<=targetDistance*0.15: #Just below target, in case of undershoot.
-            print("Reached target")
-            break;
-        time.sleep(2)
-        x = x + 1
-        if x==10:
-        	break;
-
-def condition_yaw(heading, relative=False):
+        
+def condition_yaw_death(heading, relative=False):
     if relative:
         is_relative=1 #yaw relative to direction of travel
     else:
         is_relative=0 #yaw is an absolute angle
     # create the CONDITION_YAW command using command_long_encode()
-    msg = vehicle.message_factory.command_long_encode(
+    msg = Death.message_factory.command_long_encode(
         0, 0,    # target system, target component
         mavutil.mavlink.MAV_CMD_CONDITION_YAW, #command
         0, #confirmation
@@ -162,11 +79,89 @@ def condition_yaw(heading, relative=False):
         is_relative, # param 4, relative offset 1, absolute angle 0
         0, 0, 0)    # param 5 ~ 7 not used
     # send command to vehicle
-    vehicle.send_mavlink(msg)
+    Death.send_mavlink(msg)
+
+def arm_and_takeoff_taxes(aTargetAltitude):
+    """
+    Arms vehicle and fly to aTargetAltitude.
+    """
+
+    print("Basic pre-arm checks")
+    # Don't let the user try to arm until autopilot is ready
+    while not Taxes.is_armable:
+        print(" Waiting for vehicle to initialise...")
+        time.sleep(1)
+
+        
+    print("Arming motors")
+    # Copter should arm in GUIDED mode
+    Taxes.mode = VehicleMode("GUIDED")
+    Taxes.armed = True
+
+    while not Taxes.armed:      
+        print(" Waiting for arming...")
+        time.sleep(1)
+
+    print("Taking off!")
+    Taxes.simple_takeoff(aTargetAltitude) # Take off to target altitude
+
+    # Wait until the vehicle reaches a safe height before processing the goto (otherwise the command 
+    #  after Vehicle.simple_takeoff will execute immediately).
+    while True:
+        print(" Altitude: ", Taxes.location.global_relative_frame.alt)      
+        if Taxes.location.global_relative_frame.alt>=aTargetAltitude*0.95: #Trigger just below target alt.
+            print("Reached target altitude")
+            break
+        time.sleep(1)
+
+def send_global_velocity_taxes(velocity_x, velocity_y, velocity_z, duration):
+    """
+    Move vehicle in direction based on specified velocity vectors.
+    """
+    msg = Taxes.message_factory.set_position_target_global_int_encode(
+        0,       # time_boot_ms (not used)
+        0, 0,    # target system, target component
+        mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT_INT, # frame
+        0b0000111111000111, # type_mask (only speeds enabled)
+        0, # lat_int - X Position in WGS84 frame in 1e7 * meters
+        0, # lon_int - Y Position in WGS84 frame in 1e7 * meters
+        0, # alt - Altitude in meters in AMSL altitude(not WGS84 if absolute or relative)
+        # altitude above terrain if GLOBAL_TERRAIN_ALT_INT
+        velocity_x, # X velocity in NED frame in m/s
+        velocity_y, # Y velocity in NED frame in m/s
+        velocity_z, # Z velocity in NED frame in m/s
+        0, 0, 0, # afx, afy, afz acceleration (not supported yet, ignored in GCS_Mavlink)
+        0, 0)    # yaw, yaw_rate (not supported yet, ignored in GCS_Mavlink)
+
+    # send command to vehicle on 1 Hz cycle
+    for x in range(0,duration):
+        Taxes.send_mavlink(msg)
+        time.sleep(1)
+        
+def condition_yaw_taxes(heading, relative=False):
+    if relative:
+        is_relative=1 #yaw relative to direction of travel
+    else:
+        is_relative=0 #yaw is an absolute angle
+    # create the CONDITION_YAW command using command_long_encode()
+    msg = Taxes.message_factory.command_long_encode(
+        0, 0,    # target system, target component
+        mavutil.mavlink.MAV_CMD_CONDITION_YAW, #command
+        0, #confirmation
+        heading,    # param 1, yaw in degrees
+        0,          # param 2, yaw speed deg/s
+        1,          # param 3, direction -1 ccw, 1 cw
+        is_relative, # param 4, relative offset 1, absolute angle 0
+        0, 0, 0)    # param 5 ~ 7 not used
+    # send command to vehicle
+    Taxes.send_mavlink(msg)
 
 def RealSense_Code():
     aruco == 0
     #Add more code here
+
+Death = connect('/dev/ttyAMA0', wait_ready=True, baud=57600)
+Taxes = connect('/dev/ttyUSB0', baud=57600)
 
 rows1 =[] #Rows for storing coarse positioning data
 rows2 =[] #Rows for storiing fine positioning data
@@ -178,8 +173,10 @@ type(RealSense)
     
     
 
-arm_and_takeoff(5)
-vehicle.mode = VehicleMode("GUIDED")
+arm_and_takeoff_taxes(1)
+arm_and_takeoff_death(4)
+Taxes.mode = VehicleMode("STABILIZE")
+Death.mode = VehicleMode("GUIDED")
 time.sleep(1)
 
 aruco = 0 #Aruco marker starts as undetected
@@ -194,7 +191,7 @@ while True:
 
         location1 = (rows1[-1])
         point = LocationGlobalRelative(float(location1[0]), float(location1[1]), float(location1[2]))
-        vehicle.simple_goto(point, groundspeed=1)
+        Death.simple_goto(point, groundspeed=1)
         print("Locating Taxes")
         time.sleep(1)
     elif aruco == 1: #Aruco marker detected
@@ -204,12 +201,14 @@ while True:
             rows2.append(row)
 
         location2 = (rows2[-1])
-        condition_yaw(0)
-        send_global_velocity(float(location2[0]),float(location2[2]),float(location2[2]),1)
+        condition_yaw_death(0)
+        condition_yaw_taxes(0)
+        send_global_velocity_death(float(location2[0]),float(location2[2]),float(location2[2]),1)
     elif attachment == 1: #Limit switch has indicated attachment
         break
 
 print("Attachment Successful. Moving to Landing Zone")
-goto(5,5)
+#goto(5,5)
 print("Landing")
-vehicle.mode = VehicleMode("LAND")
+Death.mode = VehicleMode("LAND")
+Taxes.mode = VehicleMode("LAND")
